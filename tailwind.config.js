3e338c2f418a3acae4ddb75b3d4b1ecf94b25d4f
Sylwia <chedkowska0@gmail.com>
2025-01-13 13:28:42 +0100

    add some styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: ['class'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      },
+      colors: {
+        primary: '#0077c0',
+        secondary: '#6cb4ee',
+        tertiary: '#F0F8FF'
+      }
+    }
   },
   // eslint-disable-next-line no-undef
-  plugins: [require("daisyui"), require("tailwindcss-animate")],
+  plugins: [require('daisyui'), require('tailwindcss-animate')],
   daisyui: {
-    themes: [
-      "nord"
-    ]
-  },
+    themes: ['nord']
+  }
 }
-
